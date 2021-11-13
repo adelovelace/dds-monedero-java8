7feea01887f14fb413d729e48a570201fed3a7e3
Washington Genaro Cotera Mendoza <60835916+wgcotera@users.noreply.github.com>
2021-11-12 21:41:47 -0500

    Update CuentaBancaria.java

--- src/main/java/dds/monedero/model/CuentaBancaria.java
+++ src/main/java/dds/monedero/model/CuentaBancaria.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dds.monedero.model;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author ERWIN AURIA
- */
 public class CuentaBancaria {
     
     private double saldo;
